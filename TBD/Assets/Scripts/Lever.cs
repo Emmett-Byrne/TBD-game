29b6591351883a35d6c49c95724ebb18553c39fd
MateuszStepien0 <c00230799@itcarlow.ie>
2019-01-26 18:12:58 +0000

    hi, tis my shit

--- TBD/Assets/Scripts/Lever.cs
+++ TBD/Assets/Scripts/Lever.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class trigger : MonoBehaviour
+public class Lever : MonoBehaviour
 {
     public GameObject door;
+  //  private Rigidbody rb;
+
+    float angle = -33f;
+
+    void Start()
+    {
+   //     rb = GetComponent<Rigidbody>();
+    }
 
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
             door.SetActive(true);
+   //         rb.rotation.eulerAngles.Set(0.617f, -4.102f, -33.671f);
         }
     }
 }
