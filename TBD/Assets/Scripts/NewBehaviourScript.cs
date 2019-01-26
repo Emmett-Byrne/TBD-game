84a85b60f4ec9ab3baa430514bcfc523044c1e18
davidc00185501 <c00185501@itcarlow.ie>
2019-01-26 14:50:41 +0000

    jump and triggers

---
+++ TBD/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float speed = 10.0f;
+    float jump = 0.0f;
+    bool isGrounded = false;
+    public GameObject door;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+    private void onTriggerEnter()
+    {
+        door.SetActive(true);
+    }
+
+    void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void Update()
+    { 
+        if (Input.GetKey(KeyCode.Space) && isGrounded == true)
+        {
+            jump = 10f;
+            isGrounded = false;
+        }
+        else
+        {
+            jump = 0f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHori = Input.GetAxis("Horizontal");
+        // float moveVerti = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(moveHori, jump, 0.0f) * speed;
+
+        rb.AddForce(movement);
+    }
+}
---
+++ TBD/Assets/Scripts/trigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger : MonoBehaviour {
+
+    public GameObject door;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            door.SetActive(true);
+        }
+    }
+}
