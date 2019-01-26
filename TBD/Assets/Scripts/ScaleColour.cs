4cb8f238687779814b7b2c8f1b1ef3ceb2cf57f0
Emmett Byrne <c00220950@itcarlow.ie>
2019-01-26 14:30:23 +0000

    creted scaling colour script

---
+++ TBD/Assets/Scripts/ScaleColour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleColour : MonoBehaviour
+{
+    public int minHeight;
+    public int maxHeight;
+    public Color startColour;
+    public Color endColour;
+    Color curruntColour;
+
+    public Rigidbody other;
+
+    Renderer m_rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rend = GetComponent<Renderer>();
+        curruntColour = startColour;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scaler = other.position.y / (maxHeight - minHeight);
+
+        curruntColour = startColour + ((endColour - startColour) * scaler);
+
+        m_rend.material.SetColor("_Color", curruntColour);
+    }
+}
