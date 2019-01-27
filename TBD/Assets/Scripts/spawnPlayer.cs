e4b0d077450334a15480968555ed8e5584bdd419
Emmett Byrne <c00220950@itcarlow.ie>
2019-01-27 12:38:55 +0000

    player spawn anim

---
+++ TBD/Assets/Scripts/spawnPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPlayer : MonoBehaviour
+{
+    public GameObject particles;
+    public GameObject player;
+    private IEnumerator coroutine;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coroutine = spawnWait(0.5f);
+        StartCoroutine(coroutine);
+        player.SetActive(false);
+        particles.transform.position = player.transform.position;
+        particles.SetActive(true);
+    }
+
+    IEnumerator spawnWait(float time)
+    {
+        yield return new WaitForSeconds(time);
+        player.SetActive(true);
+    }
+}
